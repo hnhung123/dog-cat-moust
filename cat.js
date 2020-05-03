a6e1df916aaf6e2d98b449d7fd11974e8276a59e
unknown <you@example.com>
2020-05-04 01:01:31 +0800

    Improve cat eat method

--- cat.js
+++ cat.js
@@ -1,6 +1,15 @@
-function Cat(){
+var Mouse = require('./Mouse')
+
+function Cat() {
     this.stomach = [];
 
+}
+Cat.prototype.eat = function (animal) {
+    if (animal instanceof Mouse) {
+        this.stomach.push(animal)
+    }else{
+        throw new Error('Can only eat Mouse!')
+    }
 }
 
 module.exports = Cat;
--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 var Dog = require ('./Dog');
 var Cat = require('./Cat');
+var Mouse = require('./Mouse')
 
+var mouse = new Mouse('Mickey')
 var dog = new Dog('Tom');
-var tom = new Cat()
-dog.eat(tom)
-dog.sayHi()
+var cat = new Cat()
+dog.eat(cat);
+try{
+    cat.eat(dog);
+}catch(error){
+    console.log('Error while cat eating a dog')
+}
+dog.sayHi();
+console.log(cat)
